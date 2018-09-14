2b6eed27b796b9cc399040e47e56b37082ebc59f
Carlos Antonio Sonora <sonorac@mx1.ibm.com>
2018-09-14 11:01:17 -0500

    Updating new file

--- src/App.js
+++ src/App.js
@@ -10,20 +10,21 @@ class App extends Component {
             {name: 'Antonio', age: 22},
             {name: 'Monica', age: 31}
         ],
-        otherState: 'Some other value'
+        otherState: 'Some other value',
+        showPersons: false
     } //Can only be build on components done like this, extending Component
 
-    switchNameHandler = (newName) => {
-        //console.log('Was clicked!');
-        // DON'T DO THIS: this.state.persons[0].name = 'Carlitos';
-        this.setState({
-            persons: [
-                {name: newName, age: 31},
-                {name: 'Toñito', age: 25},
-                {name: 'Moni', age: 31}
-            ]
-        });
-    }
+    // switchNameHandler = (newName) => {
+    //     //console.log('Was clicked!');
+    //     // DON'T DO THIS: this.state.persons[0].name = 'Carlitos';
+    //     this.setState({
+    //         persons: [
+    //             {name: newName, age: 31},
+    //             {name: 'Toñito', age: 25},
+    //             {name: 'Moni', age: 31}
+    //         ]
+    //     });
+    // }
 
     nameChangeHandler = (event) => {
         this.setState({
@@ -33,6 +34,17 @@ class App extends Component {
                 {name: 'Moni', age: 31}
             ]
         });
+    }
+
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
+
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     }
 
     render() {
@@ -45,24 +57,43 @@ class App extends Component {
             cursor: 'pointer'
         };
         
+        let persons = null;
+
+        if(this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                      return <Person 
+                        name={person.name} 
+                        age={person.age}
+                        click={() => this.deletePersonHandler(index)} />  //Cuando se tiene argumentos en una función, se manda ejecutar con el () =>, si no tiene argumentos, se manda llamar sin el () =>
+                    })}
+                    {/* The above lines replaces this block
+                    <Person 
+                        name={this.state.persons[0].name} 
+                        age={this.state.persons[0].age} />
+                    <Person 
+                        name={this.state.persons[1].name} 
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Chuck!')} 
+                        changed={this.nameChangeHandler} >My Hobbies: MMA</Person>
+                    <Person 
+                        name={this.state.persons[2].name} 
+                        age={this.state.persons[2].age} /> */}
+                </div> 
+            );
+        }
+
         return ( 
             <div className = "App"> 
                 <h1> Hi, I 'm a React App</h1> 
                 <p>This is really working!</p>
                 <button 
                     style={style}
-                    onClick={() => this.switchNameHandler('¡Carlitos!')}>Switch Name</button> 
-                <Person 
-                    name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age} />
-                <Person 
-                    name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Chuck!')} 
-                    changed={this.nameChangeHandler} >My Hobbies: MMA</Person>
-                <Person 
-                    name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age} />
+                    onClick={this.togglePersonsHandler}>Toggle Persons</button> 
+
+                {/* Condition ? if true : if false //Esto es una expresión ternaria */}
+                {persons}                
             </div>
         );
     }
