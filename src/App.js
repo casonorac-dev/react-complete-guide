2a1983fad9d70ce915ce7723ab34306b55d6c77f
Carlos Antonio Sonora <sonorac@mx1.ibm.com>
2018-10-03 10:41:11 -0500

    Last update from course

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Person from './Person/Person';
-import './App.css';
+import classes from './App.css';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
     state = {
@@ -67,66 +68,47 @@ class App extends Component {
 
     render() {
         //IMPORTANTE: () => this.switchNameHandler('¡Carlitos!') esta sintaxis es conveniente pero puede ser ineficiente, mejor usar bind
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
         
         let persons = null;
+        let btnClass = '';
 
         if(this.state.showPersons) {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                      return <Person 
-                        name={person.name} 
-                        age={person.age}
-                        click={() => this.deletePersonHandler(index)}
-                        key={person.id}
-                        changed={(event) => this.nameChangeHandler(event, person.id)} />  //Cuando se tiene argumentos en una función, se manda ejecutar con el () =>, si no tiene argumentos, se manda llamar sin el () =>
+                        return <ErrorBoundary key={person.id}>
+                            <Person 
+                                name={person.name} 
+                                age={person.age}
+                                click={() => this.deletePersonHandler(index)}
+                                key={person.id}
+                                changed={(event) => this.nameChangeHandler(event, person.id)} />  
+                        </ErrorBoundary>
+                        //Cuando se tiene argumentos en una función, se manda ejecutar con el () =>, si no tiene argumentos, se manda llamar sin el () =>
                     })}
-                    {/* The above lines replaces this block
-                    <Person 
-                        name={this.state.persons[0].name} 
-                        age={this.state.persons[0].age} />
-                    <Person 
-                        name={this.state.persons[1].name} 
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Chuck!')} 
-                        changed={this.nameChangeHandler} >My Hobbies: MMA</Person>
-                    <Person 
-                        name={this.state.persons[2].name} 
-                        age={this.state.persons[2].age} /> */}
                 </div> 
             );
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            }
+            btnClass = classes.Red;
         }
 
-        const classes = [];
+        const assignedClasses = [];
         if(this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
         if(this.state.persons.length <= 1) {
-            classes.push('bold')
+            assignedClasses.push(classes.bold);
         }
 
         return ( 
-            <div className = "App"> 
+            <div className ={classes.App}> 
                 <h1> Hi, I 'm a React App</h1> 
-                <p className={classes.join(' ')}>This is really working!</p>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
                 <button 
-                    style={style}
-                    onClick={this.togglePersonsHandler}>Toggle Persons</button> 
-
-                {/* Condition ? if true : if false //Esto es una expresión ternaria */}
+                    className={btnClass}
+                    onClick={this.togglePersonsHandler}>
+                    Toggle Persons
+                </button> 
+                    {/* Condition ? if true : if false //Esto es una expresión ternaria */}
                 {persons}                
             </div>
         );
